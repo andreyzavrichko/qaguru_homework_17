698bfb4314f8126e9abf87a5d1ba852a50e0b949
Andrey Zavrichko <andrey@zavrichko.dev>
2022-02-26 21:32:30 +0200

    add tests reqres.in

--- src/test/java/ru/zavrichko/ReqresTests.java
+++ src/test/java/ru/zavrichko/ReqresTests.java
@@ -7,59 +7,103 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.CoreMatchers.is;
 
 public class ReqresTests {
-    @Test
-    void successfulLogin() {
-        /*
-        Request url:
-        https://reqres.in/api/login
-        Data:
-        {
-            "email": "eve.holt@reqres.in",
-            "password": "cityslicka"
-        }
-        Response:
-        {
-            "token": "QpwL5tke4Pnpja7X4"
-        }
-         */
 
-        String data = "{ \"email\": \"eve.holt@reqres.in\", " +
-                "\"password\": \"cityslicka\" }";
+    @Test
+    void successfulRegister() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
 
         given()
                 .contentType(JSON)
                 .body(data)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("https://reqres.in/api/register")
                 .then()
                 .statusCode(200)
+                .body("id", is(4))
                 .body("token", is("QpwL5tke4Pnpja7X4"));
     }
-
     @Test
-    void negativeLogin() {
-        /*
-        Request url:
-        https://reqres.in/api/login
-        Data:
-        {
-            "email": "eve.holt@reqres.in"
-        }
-        Response:
-        {
-            "error": "Missing password"
-        }
-         */
-
-        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+    void unsuccessfulRegister() {
+        String data = "{ \"email\": \"sydney@fife\" }";
 
         given()
                 .contentType(JSON)
                 .body(data)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("https://reqres.in/api/register")
                 .then()
                 .statusCode(400)
                 .body("error", is("Missing password"));
+    }
+
+    @Test
+    void delayedResponse() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?delay=3")
+                .then()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("data.email[0]", is("george.bluth@reqres.in"));
+    }
+
+    @Test
+    void createUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void listUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("data.email[0]", is("michael.lawson@reqres.in"));
+    }
+
+    @Test
+    void singleUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+    @Test
+    void singleUserNotFound() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void listResource() {
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .statusCode(200)
+                .body("data.id[0]", is(1))
+                .body("data.name[0]", is("cerulean"))
+                .body("data.name[0]", is("cerulean"))
+                .body("data.year[0]", is(2000))
+                .body("data.color[0]", is("#98B2D1"))
+                .body("data.pantone_value[0]", is("15-4020"));
     }
 }
